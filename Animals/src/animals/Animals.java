5f28c22a8f2e2926bd292ff2cb3d31ef97517eb0
LEONARDO MOURA <2016103732@UC.ACAD>
2017-04-19 10:33:42 -0300

    Created a cat!

--- Animals/src/animals/Animals.java
+++ Animals/src/animals/Animals.java
@@ -9,17 +9,25 @@ public class Animals {
         bichos bicho = new bichos();
         
         bicho.Set("Floppy");
-        bicho.Set(1.0);
+        bicho.Set(2.0);
         
         bicho.Talk();
         
         cachorro dog = new cachorro();
         
         dog.Set(true);
-        
-        
+        dog.Set("Snoppy");
         System.out.println("Eu sou um cachorro: " + dog.GetBehaviour());
+        dog.Talk();
         
+        gato cat = new gato();
+        
+        cat.Set(true);
+        cat.Set("Flasky");
+        cat.Set(1.0);
+        
+        cat.Talk();
+        System.out.println("Eu sou um gato: " + cat.GetVaccine());
         
     }
     
--- Animals/src/bichos/gato.java
+++ Animals/src/bichos/gato.java
@@ -1,5 +1,18 @@
 package bichos;
 
-public class gato {
+public class gato extends bichos {
+    private boolean vaccinated;
     
+    public void Set(boolean v){
+        this.vaccinated = v;
+    }
+    
+    public String GetVaccine(){        
+        if(this.vaccinated) return "Vacinado";        
+        return "Não vacinado";
+    }
+    
+    public void Talk(){
+        System.out.println("Miau, meu nome é "+ this.GetNome() + "!");
+    }
 }
